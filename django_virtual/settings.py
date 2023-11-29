ef24cc2a08fd925c3b72ff88ff71e7d87811dded
EricG <ericrivas192@gmail.com>
2023-11-28 20:28:30 -0600

    ingreso de datos al db

--- django_virtual/settings.py
+++ django_virtual/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pedidos'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'django_virtual.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
+        'NAME': 'db new',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
--- pedidos/models.py
+++ pedidos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nombre_completo=models.CharField(max_length=50)
+    dpi=models.IntegerField()
+    nit=models.IntegerField()
+    telefono=models.CharField(max_length=14)
+    email=models.EmailField()
+
+class Proveedor(models.Model):
+    nombre_pro=models.CharField(max_length=30)
+    codgio_pro=models.IntegerField()
+    
+class Articulo(models.Model):
+    articulo=models.CharField(max_length=20)
+    marca=models.CharField(max_length=20)
+    modelo=models.CharField(max_length=20)
+    precio_venta=models.IntegerField()
+    precio_compra=models.IntegerField()
